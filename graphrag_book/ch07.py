68aaaaa6ac749adc4c9be8b9bc24a3d1ca112d9d
Amirhossein Layegh <amlk@kth.se>
2025-07-11 11:58:31 +0200

    chapter 07

--- graphrag_book/ch07.py
+++ graphrag_book/ch07.py
@@ -2,7 +2,7 @@ from utils import neo4j_driver, chat, chunk_text, embed, num_tokens_from_string
 from dotenv import load_dotenv
 import os
 import requests
-from ch07_tools import create_extraction_prompt, parse_extraction_output, import_nodes_query, import_relationships_query
+from ch07_tools import create_extraction_prompt, parse_extraction_output, import_nodes_query, import_relationships_query, get_summarize_prompt, import_entity_summary, import_rels_summary
 from typing import List
 from tqdm import tqdm
 import neo4j
@@ -93,7 +93,63 @@ def query_relationship_description(driver: neo4j.Driver):
                                       RETURN n.name as source, m.name as target, countOfRels, collect(r.description) as description
                                       """)
     print([el.data() for el in data])
-            
+    
+def summarize_candidate_entities(driver: neo4j.Driver):
+    candidate_to_summarize, _, _ = driver.execute_query("""
+                                                        MATCH (e:__Entity__) WHERE size(e.description) > 1
+                                                        RETURN e.name AS entity_name, e.description AS description_list
+                                                        """)
+    summaries = []
+    for en in tqdm(candidate_to_summarize, desc="Summarizing entities"):
+        messages = [
+            #{"role": "system", "content": "You are a helpful assistant that summarizes the description of an entity."},
+            {"role": "user", "content": get_summarize_prompt(en["entity_name"], en["description_list"])}
+        ]
+        response = chat(messages, model="gpt-4o-mini")
+        summaries.append({"entity_name": en["entity_name"], "summary": response})
+    return summaries
+
+def import_summaries_to_neo4j(driver: neo4j.Driver, summaries: List[dict]):
+    import_entity_summary(driver, summaries)
+    
+def query_summaries(driver: neo4j.Driver):
+    data, _, _ = driver.execute_query("""
+                                      MATCH (e:__Entity__)
+                                      WHERE e.name = 'ATLAS'
+                                      RETURN e.name AS entity_name, e.summary AS summary
+                                      """)
+    print([el.data() for el in data])   
+
+def summarize_candidate_relationships(driver: neo4j.Driver):
+    candidate_to_summarize, _, _ = driver.execute_query("""
+                                                        MATCH (s:__Entity__)-[r:RELATIONSHIP]-(t:__Entity__)
+                                                        WHERE id(s) < id(t)
+                                                        WITH s.name AS source, t.name AS target, collect(r.description) AS description_list, count(*) as count
+                                                        WHERE count > 1
+                                                        RETURN source, target, description_list
+                                                        """)
+    summaries = []
+    for rel in tqdm(candidate_to_summarize, desc="Summarizing relationships"):
+        entity_name = f"{rel['source']} relationship to {rel['target']}"
+        messages = [
+            #{"role": "system", "content": "You are a helpful assistant that summarizes the description of a relationship."},
+            {"role": "user", "content": get_summarize_prompt(entity_name, rel["description_list"])}
+        ]
+        response = chat(messages, model="gpt-4o-mini")
+        summaries.append({"source": rel["source"], "target": rel["target"], "summary": response})
+    return summaries
+
+def import_relationship_summaries_to_neo4j(driver: neo4j.Driver, summaries: List[dict]):
+    import_rels_summary(driver, summaries)
+    
+def query_relationship_summaries(driver: neo4j.Driver):
+    data, _, _ = driver.execute_query("""  
+                                      MATCH (s:__Entity__)-[r:SUMMARIZED_RELATIONSHIP]-(t:__Entity__)
+                                      RETURN s.name AS source, t.name AS target, r.summary AS summary
+                                      """)
+    print([el.data() for el in data])
+    
+
 if __name__ == "__main__":
     books = load_data_and_chunk_into_books()
     token_count(books)
@@ -105,6 +161,12 @@ if __name__ == "__main__":
     #store_to_neo4j(driver, chunked_books)
     #query_database(driver)
     #query_person_description(driver)
-    query_relationship_description(driver)
+    #query_relationship_description(driver)
+    #summaries = summarize_candidate_entities(driver)
+    #import_summaries_to_neo4j(driver, summaries)
+    #query_summaries(driver)
+    #summaries = summarize_candidate_relationships(driver)
+    #import_relationship_summaries_to_neo4j(driver, summaries)
+    query_relationship_summaries(driver)
     driver.close()
             
